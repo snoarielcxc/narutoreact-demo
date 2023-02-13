6f1d9c860eda12071cfbf38b8a697b17c9808074
Ariel <arielyu.tc@gmail.com>
2023-02-13 13:53:35 +0800

    onclicl

--- src/components/characterCard.tsx
+++ src/components/characterCard.tsx
@@ -2,7 +2,7 @@ import "./characterCard.css";
 
 function CharacterCard({ character }: any) {
   return (
-    <div className="card">
+    <div className="card" onClick={() => alert(JSON.stringify(character))}>
       <img className="cardImg" src={character.images[0]} />
       <div className="cardText">{character.name}</div>
     </div>
