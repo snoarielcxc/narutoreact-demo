99ec9ff94801e092341135f35b9a184220406d41
Ariel <arielyu.tc@gmail.com>
2023-02-13 11:56:09 +0800

    user State

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,25 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
   useEffect(() => {
     async function fetchNaruto() {
       let response = await fetch(
         "https://naruto-api.fly.dev/api/v1/characters"
       );
-      let data = await response.json();
-      console.log(data);
+      let result = await response.json();
+      setData(result);
+      console.log(result);
     }
     fetchNaruto();
   }, []);
 
-  return <div>Hello</div>;
+  return (
+    <div>
+      <div>Hello</div>
+      <div>{JSON.stringify(data)}</div>
+    </div>
+  );
 }
 
 export default App;
